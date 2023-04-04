9cbc17d01870bdcb96052614f59e597832aa384a
Sebastian <SebasTian.C.C1418@gmail.com>
2023-04-03 21:35:53 -0500

    Third Class

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -6,6 +6,7 @@ from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 class Dinosaur(Sprite):
     X_POS = 80
     Y_POS = 310
+    Y_POS_DUCK = 340
     JUMP_SPEED = 8.5
 
     def __init__(self):
@@ -30,13 +31,14 @@ class Dinosaur(Sprite):
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_duck = True
+            self.dino_run = False
+            self.dino_jump = False
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
-
-        if user_input[pygame.K_DOWN]:
-            self.dino_duck = True
-            self.dino_run = False
+            self.dino_duck = False
         
 
         if self.step_index >= 10:
@@ -65,5 +67,5 @@ class Dinosaur(Sprite):
         self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = self.X_POS
-        self.dino_rect.y = self.Y_POS + 30
+        self.dino_rect.y = self.Y_POS_DUCK
         self.step_index += 1 
