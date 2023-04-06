d5241d88a3bdbe0d1defd985159b1d3d76d177af
Sebastian <SebasTian.C.C1418@gmail.com>
2023-04-05 23:49:17 -0500

    fifth class

---
+++ dino_runner/components/power_ups/power_up_manager.py
@@ -0,0 +1,40 @@
+import pygame
+import random
+
+from dino_runner.components.power_ups.shield import Shield
+from dino_runner.utils.constants import SHIELD_TYPE
+
+class PowerUpManager:
+    def __init__(self):
+        self.power_ups = []
+        self.duration = random.randint(3, 5)
+        self.appear_when = random.randint(50, 70)
+
+    def update(self, game):
+        if len(self.power_ups) == 0 and self.appear_when == game.score.count:
+            self.generate_power_up()
+        for power_up in self.power_ups:
+            power_up.update(game.game_speed, self.power_ups)
+            if game.player.dino_rect.colliderect(power_up.rect):
+                power_up.start_time = pygame.time.get_ticks()
+                game.player.has_power_up = True
+                game.player.type = SHIELD_TYPE
+                game.player.power_up_time = power_up.start_time + (self.duration * 1000)
+                
+                self.power_ups.remove(power_up)
+
+
+
+    def draw(self, screen):
+        for power_up in self.power_ups:
+            power_up.draw(screen)
+
+    def reset_power_ups(self):
+        self.power_ups = []
+        self.appear_when = random.randint(50, 70)
+
+    def generate_power_up(self):
+        self.appear_when += random.randint(200, 300)
+        power_up = Shield()
+        self.power_ups.append(power_up)
+        
